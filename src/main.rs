492b78d0b0e658f9b70f4aa903295400c3bd342d
Kevin Wang <kwangsan@gmail.com>
2020-07-18 20:35:21 -0400

    WS + warp implementation

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,185 @@
-use redis::aio::MultiplexedConnection;
-use redis::{Commands, Connection, ConnectionAddr, ConnectionInfo};
-use std::env;
+// #![deny(warnings)]
+use std::collections::HashMap;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+
+use futures::{FutureExt, StreamExt};
+use serde_derive::{Deserialize, Serialize};
+use tokio::sync::{mpsc, RwLock};
+use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
-mod filters;
-mod handlers;
-mod models;
+/// Our global unique user id counter.
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
-/// Provides a RESTful web server managing some Todos.
+/// Our state of currently connected users.
 ///
-/// API will be:
-///
-/// - `GET /todos`: return a JSON list of Todos.
-/// - `POST /todos`: create a new Todo.
-/// - `PUT /todos/:id`: update a specific Todo.
-/// - `DELETE /todos/:id`: delete a specific Todo.
+/// - Key is their id
+/// - Value is a sender of `warp::ws::Message`
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
+
 #[tokio::main]
 async fn main() {
-    let redis_host = env::var("REDIS_HOST").unwrap_or("localhost".to_string());
-    let redis_connection_params = format!("redis://{}:6379", redis_host);
-
-    // ⚠️ ConnectionAddr::Tcp doesn't need the 'redis://' prefix/protocol
-    let addr = ConnectionAddr::Tcp(format!("{}", redis_host), 6379);
-    let connection_info = ConnectionInfo {
-        addr: Box::new(addr),
-        db: 0,
-        passwd: None,
-    };
-    let client = redis::Client::open(redis_connection_params).unwrap();
-    // let mut connection = client.get_multiplexed_async_std_connection().await.unwrap();
-
-    // let res: redis::RedisResult<isize> = connection.incr("count", 1);
-    // println!("res = {}", res.unwrap());
-
-    if env::var_os("RUST_LOG").is_none() {
-        // Set `RUST_LOG=todos=debug` to see debug logs,
-        // this only shows access logs.
-        env::set_var("RUST_LOG", "todos=info");
-    }
     pretty_env_logger::init();
 
-    let db = models::blank_db();
+    // Keep track of all connected users, key is usize, value
+    // is a websocket sender.
+    let users_arc = Users::default();
+    let users_arc_2 = users_arc.clone();
+    // Turn our "state" into a new Filter...
+    let users = warp::any().map(move || users_arc.clone());
+    let users_2 = warp::any().map(move || users_arc_2.clone());
 
-    let api = filters::todos(db, connection_info);
+    // GET /users
+    let get_users = warp::path("users").and(users_2).map(|u| {
+        println!("GET /users, {:?}", u);
+        // GET /users, RwLock { s: Semaphore { permits: 64 }, c: UnsafeCell }
 
-    // View access logs by setting `RUST_LOG=todos`.
-    let routes = api.with(warp::log("todos"));
-    // Start up the server...
+        // FIXME
+        warp::reply::json(&1)
+    });
+
+    // GET /chat -> websocket upgrade
+    let chat = warp::path("chat")
+        // The `ws()` filter will prepare Websocket handshake...
+        .and(warp::ws())
+        .and(users)
+        .map(|ws: warp::ws::Ws, users| {
+            println!("GET /chat");
+            // This will call our function if the handshake succeeds.
+            ws.on_upgrade(move |socket| user_connected(socket, users))
+        });
+
+    // GET / -> index html
+    let index = warp::path::end().map(|| {
+        println!("GET /");
+        warp::reply::html(INDEX_HTML)
+    });
+    let health = warp::path("health").map(|| {
+        println!("GET /health");
+        warp::reply::json(&"ok".to_string())
+    });
+
+    let routes = index.or(chat).or(health).or(get_users);
+
     warp::serve(routes).run(([0, 0, 0, 0], 3000)).await;
 }
+
+async fn user_connected(ws: WebSocket, users: Users) {
+    // Use a counter to assign a new unique ID for this user.
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+
+    eprintln!("new chat user: {}", my_id);
+
+    // Split the socket into a sender and receive of messages.
+    let (user_ws_tx, mut user_ws_rx) = ws.split();
+
+    // Use an unbounded channel to handle buffering and flushing of messages
+    // to the websocket...
+    let (tx, rx) = mpsc::unbounded_channel();
+    tokio::task::spawn(rx.forward(user_ws_tx).map(|result| {
+        if let Err(e) = result {
+            eprintln!("websocket send error: {}", e);
+        }
+    }));
+
+    // Save the sender in our list of connected users.
+    users.write().await.insert(my_id, tx);
+
+    // Return a `Future` that is basically a state machine managing
+    // this specific user's connection.
+
+    // Make an extra clone to give to our disconnection handler...
+    let users2 = users.clone();
+
+    // Every time the user sends a message, broadcast it to
+    // all other users...
+    while let Some(result) = user_ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("websocket error(uid={}): {}", my_id, e);
+                break;
+            }
+        };
+        user_message(my_id, msg, &users).await;
+    }
+
+    // user_ws_rx stream will keep processing as long as the user stays
+    // connected. Once they disconnect, then...
+    user_disconnected(my_id, &users2).await;
+}
+
+async fn user_message(my_id: usize, msg: Message, users: &Users) {
+    // Skip any non-Text messages...
+    let msg = if let Ok(s) = msg.to_str() {
+        println!("{:?}", s);
+        s
+    } else {
+        return;
+    };
+
+    let new_msg = format!("<User#{}>: {}", my_id, msg);
+
+    // New message from this user, send it to everyone else (except same uid)...
+    for (&uid, tx) in users.read().await.iter() {
+        if my_id != uid {
+            if let Err(_disconnected) = tx.send(Ok(Message::text(new_msg.clone()))) {
+                // The tx is disconnected, our `user_disconnected` code
+                // should be happening in another task, nothing more to
+                // do here.
+            }
+        }
+    }
+}
+
+async fn user_disconnected(my_id: usize, users: &Users) {
+    eprintln!("good bye user: {}", my_id);
+
+    // Stream closed up, so remove from the user list
+    users.write().await.remove(&my_id);
+}
+
+static INDEX_HTML: &str = r#"<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Warp Chat</title>
+    </head>
+    <body>
+        <h1>Warp chat</h1>
+        <div id="chat">
+            <p><em>Connecting...</em></p>
+        </div>
+        <input type="text" id="text" />
+        <button type="button" id="send">Send</button>
+        <script type="text/javascript">
+        const chat = document.getElementById('chat');
+        const text = document.getElementById('text');
+        const uri = 'ws://' + location.host + '/chat';
+        const ws = new WebSocket(uri);
+        function message(data) {
+            const line = document.createElement('p');
+            line.innerText = data;
+            chat.appendChild(line);
+        }
+        ws.onopen = function() {
+            chat.innerHTML = '<p><em>Connected!</em></p>';
+        };
+        ws.onmessage = function(msg) {
+            message(msg.data);
+        };
+        ws.onclose = function() {
+            chat.getElementsByTagName('em')[0].innerText = 'Disconnected!';
+        };
+        send.onclick = function() {
+            const msg = text.value;
+            ws.send(msg);
+            text.value = '';
+            message('<You>: ' + msg);
+        };
+        </script>
+    </body>
+</html>
+"#;
