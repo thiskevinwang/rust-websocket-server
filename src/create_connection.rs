9a08f636eec1e3562fe855c18451e2ce1b2fbc5f
Kevin Wang <kwangsan@gmail.com>
2020-08-09 09:24:52 -0400

    refactor create_connection

--- src/create_connection.rs
+++ src/create_connection.rs
@@ -1,22 +1,16 @@
 use tokio_postgres::{Client, Error, NoTls};
 
+// static CONNECTION_STRING: &str =
+//     "host=localhost port=8080 user=postgres password='mysecretpassword'";
+static CONNECTION_STRING: &str = "postgres://postgres:mysecretpassword@localhost:8080/postgres";
+
 pub async fn create_connection() -> Result<Client, Error> {
     // @see https://docs.rs/tokio-postgres/0.5.5/tokio_postgres/config/struct.Config.html#examples
-    let (client, connection) = tokio_postgres::connect(
-        "host=localhost port=8080 user=postgres password='mysecretpassword'",
-        NoTls,
-    )
-    .await?;
-
-    info!("Client: {:?}", client);
+    let (client, connection) = tokio_postgres::connect(CONNECTION_STRING, NoTls).await?;
 
     // The connection object performs the actual communication with the database,
     // so spawn it off to run on its own.
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            error!("connection error: {}", e);
-        }
-    });
+    tokio::spawn(connection);
 
     Ok(client)
 }
